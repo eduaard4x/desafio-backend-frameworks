241266d48470e2b77c0e39bcdae1e4dec1651597
Eduarda Paz <eduardadev@outlook.com>
2026-04-23 21:08:37 -0300

    criação model java

---
+++ java-springboot/src/main/java/com/example/demo/models/User.java
@@ -0,0 +1,11 @@
+package com.example.demo.models;
+
+public class User {
+    private String name;
+    private String email;
+
+    public User() {}
+
+    public String getName() { return name; }
+    public String getEmail() { return email; }
+}
